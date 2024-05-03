1aa219b4157aab519c13fe5fb8e99741789ad093
Yamile Velazquez <yamilevelazquezgarcia@gmail.com>
2024-05-03 10:29:12 -0600

    se agregaron los enpoint para obtener un recurso y uno para crear recuersos

--- controllers/cats.ts
+++ controllers/cats.ts
@@ -1,9 +1,55 @@
 import { Request, Response, Router } from 'express';
 
+const { v4: uuidv4 } = require('uuid');
+
 const catsRouter = Router();
 
+
+let cats = [{
+    id: '429d75f6-5c54-4338-87ce-6ae6b7d50c0e',
+    name: 'pablito',
+    age: 3
+}]
+
+const generateId = () => {
+    return uuidv4();
+  };
+
 catsRouter.get('/', (req: Request, res: Response) => {
-  res.send('¡Hola desde el controlador!');
+  res.send(cats);
 });
+
+
+catsRouter.get('/:id', (req, res) => {
+  const id =(req.params.id);
+  const cat = cats.find((cat) => cat.id === id);
+
+  if (cat) {
+    res.json(cat);
+  } else {
+    res.status(404).end();
+  }
+
+});
+
+catsRouter.post('/', (req,res) => {
+    const body = req.body;
+  
+    if (!body.name || !body.age) {
+      return res.status(400).json( { error: 'nombre y edad son necesarios' } );
+    }
+  
+    const newCat = {
+      id: generateId(),
+      name: body.name,
+      age: body.age,
+    };
+  
+    cats = cats.concat(newCat);
+    res.json(newCat);
+  });
+
+
+
 
 export default catsRouter;
--- server.ts
+++ server.ts
@@ -1,14 +1,16 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 import catsRouter from './controllers/cats';
 
 const app: Express = express();
 const PORT: number = 3000;
 
-// app.get('/', (req: Request, res: Response) => {
-//   res.send('¡Hola, mundo!');
-// });
+app.use(express.json())
 
-app.use('/', catsRouter);
+app.get('/', (req: Request, res: Response) => {
+  res.send('¡Hola, mundo!');
+});
+
+app.use('/cats', catsRouter);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
