5ec3a2a386a06b1ffb302f875cc4689d9fd5700d
Susana <hernandeznerisusanaisabel@gmail.com>
2024-05-04 10:05:44 -0600

    se formateo el repositorio y el servicio de dogs

--- dog/services/ update.dogs.service.ts
+++ dog/services/ update.dogs.service.ts
@@ -1,21 +1,21 @@
 import { dogRepository, DogRepository } from "../repositorys/dogs.repository";
 
-export class UpdatedDogService{
-    private readonly repository: DogRepository;
+export class UpdatedDogService {
+  private readonly repository: DogRepository;
 
-    constructor(repository: DogRepository){
-      this.repository = repository;
-    }
+  constructor(repository: DogRepository){
+    this.repository = repository;
+  }
     
-    execute(id: string, updateDog: any){
-      const dog = this.repository.getOne(id);
+  execute(id: string, updateDog: any){
+    const dog = this.repository.getOne(id);
 
-      if(!dog){
-        throw new Error('perro no encontrado');
-      };
+    if(!dog){
+     throw new Error('perro no encontrado');
+    };
 
-      const updatedDog = {...dog, ...updateDog}
-      dogRepository.update(id, updateDog)
-      return updatedDog;
-    }
+    const updatedDog = {...dog, ...updateDog}
+    dogRepository.update(id, updateDog)
+    return updatedDog;
+  }
 }
